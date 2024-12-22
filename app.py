4db9e2fd16574030f0896b75128db825fec2eff1
IamRealYuki <124439244+IamRealYuki@users.noreply.github.com>
2024-12-22 22:29:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Jenkins! This is your app running in Docker."
+    return "А теперь изменяем текст и ждём 2 минуты."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
