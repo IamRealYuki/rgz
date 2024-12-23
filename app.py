cede61629612210bcb6a665e5ddd8bff28437b8a
IamRealYuki <124439244+IamRealYuki@users.noreply.github.com>
2024-12-23 15:15:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Не дожидаемся сборки"
+    return "Изменим текст"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
